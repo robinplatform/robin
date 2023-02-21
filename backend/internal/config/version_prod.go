34eca601e1898e2fde8a73a4576d4ea32e21344b
Karim Alibhai <karim@alibhai.co>
2023-02-21 18:50:16 -0500

    fix go gen

--- backend/internal/config/version_prod.go
+++ backend/internal/config/version_prod.go
@@ -7,12 +7,13 @@ import (
 	"fmt"
 )
 
-var (
-	//go:generate cp ../../package.json .
-	//go:embed package.json
-	rawPackageJsonRaw []byte
-	rootPackageJson   PackageJson
-)
+//go:generate cp ../../package.json .
+//go:embed package.json
+var rawPackageJsonRaw []byte
+
+// rootPackageJson is the parsed version of `rawPackageJsonRaw`, but we will
+// probably get rid of all this sillyness once we figure out stable releases.
+var rootPackageJson PackageJson
 
 func init() {
 	if err := ParsePackageJson(rawPackageJsonRaw, &rootPackageJson); err != nil {
