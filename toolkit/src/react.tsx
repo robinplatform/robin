7fdf8bf3459786840cc8ac0769a8cf46e3562372
Karim Alibhai <karim@alibhai.co>
2023-02-23 00:56:56 -0500

    fix format

--- toolkit/src/react.tsx
+++ toolkit/src/react.tsx
@@ -2,8 +2,5 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 export function renderApp(children: React.ReactNode) {
-	ReactDOM.render(
-		<>{children}</>,
-		document.getElementById('root'),
-	);
+	ReactDOM.render(<>{children}</>, document.getElementById('root'));
 }
