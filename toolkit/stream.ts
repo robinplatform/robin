34814680cfbed13b041cae51638be229871b9879
Albert Liu <albertymliu@gmail.com>
2023-03-18 20:26:54 -0700

    maybe better Stream code

--- toolkit/stream.ts
+++ toolkit/stream.ts
@@ -1,26 +1,63 @@
 let _ws: Promise<WebSocket> | null = null;
 let inFlight: Record<string, Stream> = {};
 
+async function getWs(): Promise<WebSocket> {
+	if (_ws !== null) return _ws;
+
+	let resolve: (ws: WebSocket) => void = () => {};
+
+	_ws = new Promise<WebSocket>((res) => (resolve = res));
+
+	const ws = new WebSocket('ws://localhost:9010/api/websocket');
+	ws.onclose = (evt) => {
+		console.log('close', evt.code);
+	};
+
+	ws.onmessage = (evt) => {
+		const data = JSON.parse(evt.data);
+		const stream = inFlight[data.id];
+		if (!stream) {
+			return;
+		}
+
+		if (data.kind === 'error') {
+			stream.onerror(data.error);
+		} else {
+			stream.onmessage(data);
+		}
+	};
+
+	ws.onerror = (evt) => {
+		console.log('error', evt);
+	};
+
+	ws.onopen = () => {
+		console.log('opened');
+		resolve(ws);
+	};
+
+	return _ws;
+}
+
 export class Stream {
 	onmessage: (a: any) => void = () => {};
 	onerror: (a: any) => void = () => {};
 
 	private started = false;
+	private closed = false;
 
-	constructor(
-		private readonly ws: WebSocket,
-		readonly method: string,
-		readonly id: string,
-	) {}
+	constructor(readonly method: string, readonly id: string) {}
 
-	start(data: any) {
+	async start(data: any) {
 		if (this.started) {
 			throw new Error('Already started');
 		}
 
 		this.started = true;
+		inFlight[this.id] = this;
 
-		this.ws.send(
+		const ws = await getWs();
+		ws.send(
 			JSON.stringify({
 				kind: 'call',
 				method: this.method,
@@ -30,50 +67,24 @@ export class Stream {
 		);
 	}
 
-	static async callStreamRpc(method: string, id: string): Promise<Stream> {
-		const ws = await this.getWs();
+	async close() {
+		if (!this.started) {
+			throw new Error(`hasn't started yet`);
+		}
 
-		const stream = new Stream(ws, method, id);
-		inFlight[id] = stream;
+		if (this.closed) {
+			return;
+		}
 
-		return stream;
-	}
+		this.closed = true;
 
-	static async getWs(): Promise<WebSocket> {
-		if (_ws !== null) return _ws;
-
-		let resolve: (ws: WebSocket) => void = () => {};
-
-		_ws = new Promise<WebSocket>((res) => (resolve = res));
-
-		const ws = new WebSocket('ws://localhost:9010/api/websocket');
-		ws.onclose = (evt) => {
-			console.log('close', evt.code);
-		};
-
-		ws.onmessage = (evt) => {
-			const data = JSON.parse(evt.data);
-			const stream = inFlight[data.id];
-			if (!stream) {
-				return;
-			}
-
-			if (data.kind === 'error') {
-				stream.onerror(data.error);
-			} else {
-				stream.onmessage(data);
-			}
-		};
-
-		ws.onerror = (evt) => {
-			console.log('error', evt);
-		};
-
-		ws.onopen = () => {
-			console.log('opened');
-			resolve(ws);
-		};
-
-		return _ws;
+		const ws = await getWs();
+		ws.send(
+			JSON.stringify({
+				kind: 'cancel',
+				method: this.method,
+				id: this.id,
+			}),
+		);
 	}
 }
