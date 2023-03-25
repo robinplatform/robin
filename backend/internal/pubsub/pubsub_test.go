4e8a14b46ea8f3019c45874ecd6193c75d79c3aa
Albert Liu <albertymliu@gmail.com>
2023-03-24 23:52:36 -0700

    added tests

--- backend/internal/pubsub/pubsub_test.go
+++ backend/internal/pubsub/pubsub_test.go
@@ -1,76 +1,141 @@
 package pubsub
 
 import (
+	"fmt"
 	"sync"
 	"testing"
 )
 
-func TestPubSubSimple(t *testing.T) {
-	var registry Registry
+func TestPubSubTopicCollision(t *testing.T) {
+	registry := &Registry{}
 
 	topicId := TopicId{
 		Category: "wassa",
 		Key:      "wassa",
 	}
-	topic, err := CreateTopic[string](&registry, topicId)
+	var err error
+
+	_, err = CreateTopic[string](registry, topicId)
 	if err != nil {
 		t.Fatalf("topic couldn't be created: %s", err.Error())
 	}
 
+	metaTopicId := TopicId{
+		Category: "/topics",
+		Key:      "meta",
+	}
+	_, err = CreateTopic[string](registry, metaTopicId)
+	if err == nil {
+		t.Fatalf("creating meta topic should have failed, but didn't")
+	}
+}
+
+func RunSubscriberMessages[T comparable](registry *Registry, topicId TopicId, count int, messages []T) error {
 	var wStart sync.WaitGroup
 	var wStop sync.WaitGroup
-	failChannel := make(chan string, 10)
-	defer close(failChannel)
 
-	subscriber := func(channel <-chan string) {
+	failChannel := make(chan error)
+
+	topic, err := CreateTopic[T](registry, topicId)
+	if err != nil {
+		return err
+	}
+	defer topic.Close()
+
+	subscriber := func(sub Subscription[T]) {
 		wStart.Done()
+		defer wStop.Done()
 
-		message, ok := <-channel
-		if !ok {
-			failChannel <- "failed to read from channel"
-			return
-		}
-		if message != "Hello" {
-			failChannel <- "message was wrong"
-			return
-		}
-
-		select {
-		case message, ok = <-channel:
-			failChannel <- "read too many messages"
-			return
-		default:
+		index := 0
+		for msg := range sub.Out {
+			if index > len(messages) {
+				failChannel <- fmt.Errorf("read too many messages")
+				return
+			}
+			if msg != messages[index] {
+				failChannel <- fmt.Errorf("read the wrong message")
+				return
+			}
+			index++
 		}
 
-		wStop.Done()
+		if index != len(messages) {
+			failChannel <- fmt.Errorf("read too few messages")
+		}
 	}
 
-	for i := 0; i < 10; i++ {
+	for i := 0; i < count; i++ {
 		wStart.Add(1)
 		wStop.Add(1)
 
-		sub, err := Subscribe[string](&registry, topicId)
+		sub, err := Subscribe[T](registry, topicId)
 		if err != nil {
-			t.Fatalf("error: %s", err.Error())
+			return err
 		}
 
-		go subscriber(sub.Out)
+		go subscriber(sub)
 	}
 
 	wStart.Wait()
 
-	topic.Publish("Hello")
+	for _, msg := range messages {
+		topic.Publish(msg)
+	}
 
-	endChan := make(chan bool, 1)
+	topic.Close()
 
-	go func() {
-		wStop.Wait()
-		endChan <- true
-	}()
+	wStop.Wait()
 
 	select {
-	case message := <-failChannel:
-		t.Fatalf(message)
-	case <-endChan:
+	case err := <-failChannel:
+		return err
+	default:
+		return nil
 	}
+}
+
+func TestPubSubSimple(t *testing.T) {
+	registry := &Registry{}
+
+	topicId := TopicId{
+		Category: "wassa",
+		Key:      "wassa",
+	}
+
+	RunSubscriberMessages(registry, topicId, 10, []string{
+		"Hello",
+		"Hello",
+		"Goodbye",
+	})
+}
+
+func TestPubSubParallel(t *testing.T) {
+	registry := &Registry{}
+
+	id1 := TopicId{
+		Category: "wassa",
+		Key:      "wassa",
+	}
+
+	id2 := TopicId{
+		Category: "bassa",
+		Key:      "wassa",
+	}
+
+	id3 := TopicId{
+		Category: "dassa",
+		Key:      "wassa",
+	}
+
+	go RunSubscriberMessages(registry, id1, 3, []string{
+		"Hello", "Blarg", "Goodbye",
+	})
+
+	go RunSubscriberMessages(registry, id2, 50, []bool{
+		true, false, false, false, true,
+	})
+
+	go RunSubscriberMessages(registry, id3, 7, []int{
+		1, 2, 3, 4, 5,
+	})
 }
