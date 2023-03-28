cf432ed01949f64ced93eca174cb1b1b50fc5140
Albert Liu <albertymliu@gmail.com>
2023-03-27 23:17:57 -0700

    meh

---
+++ example/my-ext/pogo/db.server.ts
@@ -0,0 +1,50 @@
+import { z } from 'zod';
+import * as fs from 'fs';
+import _ from 'lodash';
+import * as path from 'path';
+import produce from 'immer';
+import * as os from 'os';
+import { onAppStart } from '@robinplatform/toolkit/daemon';
+
+export type Species = z.infer<typeof Species>;
+export const Species = z.object({
+	number: z.number(),
+	name: z.string(),
+	megaEnergy: z.number(),
+});
+
+export type Pokemon = z.infer<typeof Pokemon>;
+export const Pokemon = z.object({
+	pokemon: z.number(),
+});
+
+export type PogoDb = z.infer<typeof PogoDb>;
+const PogoDb = z.object({
+	pokedex: z.array(Species),
+	pokemon: z.array(Pokemon),
+});
+
+const EmptyDb: PogoDb = {
+	pokedex: [],
+	pokemon: [],
+};
+
+const DB_FILE = path.join(os.homedir(), '.robin-pogo-db');
+let DB: PogoDb = EmptyDb;
+
+onAppStart(async () => {
+	try {
+		const text = await fs.promises.readFile(DB_FILE, 'utf8');
+		const data = JSON.parse(text);
+		DB = PogoDb.parse(data);
+	} catch (e) {
+		// TODO: better error handling
+		DB = EmptyDb;
+	}
+});
+
+export async function withDb(mut: (PogoDb) => void) {
+	DB = produce(DB, mut);
+
+	await fs.promises.writeFile(JSON.stringify(DB), DB_FILE);
+}
