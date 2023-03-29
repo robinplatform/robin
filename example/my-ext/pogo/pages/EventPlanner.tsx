3dea3cfc6f7a05bfe4becb1368a96ba420078d16
Albert Liu <albertymliu@gmail.com>
2023-03-29 16:36:40 -0700

    meh

--- example/my-ext/pogo/pages/EventPlanner.tsx
+++ example/my-ext/pogo/pages/EventPlanner.tsx
@@ -4,6 +4,15 @@ import { ScrollWindow } from '../components/ScrollWindow';
 import { SelectPage } from '../components/SelectPage';
 import { getUpcomingEventsRpc, PogoEvent } from '../server/leekduck.server';
 
+// Should be able to:
+// Lock a pokemon to an event
+// Search for pokemon that are good for the specific event
+// Project out mega evolutions properly, including energy spend and daily level up limit
+
+// Idea:
+// View mega pokemon evolutions in line with events; time moves
+// downwards from today, and mega evolution projections are shown speculatively.
+
 function Event({ event }: { event: PogoEvent }) {
 	return (
 		<div
