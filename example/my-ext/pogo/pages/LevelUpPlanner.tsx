6c41de3abe584545c7690bb0af0de04057f21933
Albert Liu <albertymliu@gmail.com>
2023-03-30 12:02:41 -0700

    meh

--- example/my-ext/pogo/pages/LevelUpPlanner.tsx
+++ example/my-ext/pogo/pages/LevelUpPlanner.tsx
@@ -4,40 +4,85 @@ import { useSelectOption } from '../components/EditableField';
 import { ScrollWindow } from '../components/ScrollWindow';
 import { SelectPage } from '../components/SelectPage';
 import { megaCostForTime, MegaWaitDays, MegaWaitTime } from '../domain-utils';
-import { HOUR_MS } from '../math';
+import { arrayOfN, DAY_MS, HOUR_MS } from '../math';
 import { fetchDbRpc } from '../server/db.server';
 
-export function Day({ date }: { date: Date }) {
+// Include cancel or not
+// Specify locks/planned actions
+
+function DateText({ date }: { date: Date }) {
 	return (
 		<div
 			style={{
-				position: 'relative',
+				position: 'absolute',
+				right: '2rem',
+				top: '0',
+				bottom: '0',
+				width: '8rem',
+				display: 'flex',
+				flexDirection: 'column',
+				justifyContent: 'center',
+				textAlign: 'right',
+			}}
+		>
+			{date.toDateString()}
+		</div>
+	);
+}
+
+function SmallDot() {
+	return (
+		<div
+			style={{
+				position: 'absolute',
+				top: '0',
+				bottom: '0',
+				height: '1rem',
+				width: '1rem',
+				borderRadius: '1rem',
+				backgroundColor: 'blue',
+			}}
+		/>
+	);
+}
+
+function BigDot() {
+	return (
+		<div
+			style={{
+				position: 'absolute',
+				top: '-0.5rem',
+				left: '-0.5rem',
 				height: '2rem',
 				width: '2rem',
 				borderRadius: '2rem',
 				backgroundColor: 'blue',
 			}}
+		/>
+	);
+}
+
+function DayBox({ children }: { children: React.ReactNode }) {
+	return (
+		<div
+			style={{
+				position: 'relative',
+				height: '1rem',
+				width: '1rem',
+				borderRadius: '1rem',
+				backgroundColor: 'blue',
+			}}
 		>
-			<div
-				style={{
-					position: 'absolute',
-					right: '2.5rem',
-					top: '0',
-					bottom: '0',
-					width: '8rem',
-					display: 'flex',
-					flexDirection: 'column',
-					justifyContent: 'center',
-					textAlign: 'right',
-				}}
-			>
-				{date.toDateString()}
-			</div>
+			{children}
 		</div>
 	);
 }
 
 export function LevelUpPlanner() {
+	const days = arrayOfN(10).map(
+		(i) => new Date(Date.now() + DAY_MS + i * DAY_MS),
+	);
+
 	return (
 		<div className={'col full robin-rounded robin-gap robin-pad'}>
 			<div className={'row robin-gap'}>
@@ -46,10 +91,22 @@ export function LevelUpPlanner() {
 
 			<ScrollWindow
 				className={'full'}
-				innerClassName={'col'}
-				innerStyle={{ alignItems: 'center' }}
+				innerClassName={'col robin-pad'}
+				innerStyle={{ alignItems: 'center', background: 'white', gap: '2rem' }}
 			>
-				<Day date={new Date()} />
+				<DayBox>
+					<BigDot />
+					<DateText date={new Date()} />
+				</DayBox>
+
+				{days.map((d) => {
+					return (
+						<DayBox>
+							<SmallDot />
+							<DateText date={d} />
+						</DayBox>
+					);
+				})}
 			</ScrollWindow>
 		</div>
 	);
