cc283ff792fbf6effea4d65c96845406815f8564
Albert Liu <albertymliu@gmail.com>
2023-03-30 20:57:43 -0700

    meh

--- toolkit/daemon.ts
+++ toolkit/daemon.ts
@@ -1,7 +1,16 @@
 import { Robin } from './internal/types';
+import fetch from 'isomorphic-fetch';
 
 export async function onAppStart(handler: () => Promise<void>) {
 	if (Robin.isDaemonProcess) {
 		Robin.startupHandlers.push(handler);
 	}
+}
+
+export class Topic {
+	private constructor() {}
+
+	public static async createTopic() {}
+
+	async publish() {}
 }
