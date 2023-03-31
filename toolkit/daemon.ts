c95a6d90c6110aee539304961d2b733889710c24
Albert Liu <albertymliu@gmail.com>
2023-03-30 21:34:45 -0700

    meh

--- toolkit/daemon.ts
+++ toolkit/daemon.ts
@@ -1,5 +1,6 @@
+import { z } from 'zod';
+import { request } from '.';
 import { Robin } from './internal/types';
-import fetch from 'isomorphic-fetch';
 
 export async function onAppStart(handler: () => Promise<void>) {
 	if (Robin.isDaemonProcess) {
@@ -7,10 +8,49 @@ export async function onAppStart(handler: () => Promise<void>) {
 	}
 }
 
-export class Topic {
-	private constructor() {}
+export class Topic<T> {
+	private constructor(
+		private readonly category: string,
+		private readonly key: string,
+	) {}
 
-	public static async createTopic() {}
+	// Creates a topic under the specified category and key, as a subcategory of
+	// `/app-topics/{project}/{app}/`.
+	public static async createTopic<T>(
+		category: string,
+		key: string,
+	): Promise<Topic<T>> {
+		const res = await request({
+			pathname: '/api/apps/rpc/CreateTopic',
+			resultType: z.object({ error: z.unknown().optional() }),
+			body: {
+				appId: process.env.ROBIN_APP_ID,
+				category,
+				key,
+			},
+		});
 
-	async publish() {}
+		if (res.error) {
+			throw res.error;
+		}
+
+		return new Topic<T>(category, key);
+	}
+
+	async publish(t: T) {
+		const res = await request({
+			pathname: '/api/apps/rpc/PublishToTopic',
+			resultType: z.object({ error: z.unknown().optional() }),
+			body: {
+				appId: process.env.ROBIN_APP_ID,
+				category: this.category,
+				key: this.key,
+				data: t,
+			},
+		});
+
+		if (res.error) {
+			throw res.error;
+		}
+	}
 }
