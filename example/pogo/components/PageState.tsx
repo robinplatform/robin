cfb79ac70ca5928f0e32c192818e8a6561eb9166
Albert Liu <albertymliu@gmail.com>
2023-04-05 14:54:23 -0700

    meh

--- example/pogo/components/PageState.tsx
+++ example/pogo/components/PageState.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 import { useRpcMutation, useRpcQuery } from '@robinplatform/toolkit/react/rpc';
 import { fetchDbRpc, setPageStateRpc } from '../server/db.server';
 
-const DefaultPage = 'levelup' as const;
+const DefaultPage = 'pokemon' as const;
 
 // I'm not handling errors in this file, because... oh well. Whatever. Meh.
 const PageTypes = ['pokemon', 'planner', 'tables', 'levelup'] as const;
