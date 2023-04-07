a205726c9d1295af2510e58e222cf90d6d1f48e1
Albert Liu <albertymliu@gmail.com>
2023-04-07 14:45:07 -0700

    meh

--- toolkit/stream.ts
+++ toolkit/stream.ts
@@ -1,92 +1,89 @@
+let _ws: Promise<WebSocket> | null = null;
 const inFlight: Map<string, Stream> = new Map();
 
-const getWs = (() => {
-	let _ws: Promise<WebSocket> | null = null;
+async function getWs(): Promise<WebSocket> {
+	if (_ws !== null) {
+		return _ws;
+	}
 
-	return (): Promise<WebSocket> => {
-		if (_ws !== null) {
-			return _ws;
+	console.log('making new websocket');
+	let resolveWs: (ws: WebSocket) => void = () => {};
+
+	const newWs = new Promise<WebSocket>((res) => (resolveWs = res));
+	_ws = newWs;
+
+	const ws = new WebSocket(
+		`ws://${window.location.hostname}:9010/api/websocket`,
+	);
+	ws.onclose = (evt) => {
+		console.log('close', evt.code);
+
+		// We only want to over-write if this specific websocket is the one that
+		// is currently active; otherwise, we can just die peacefully.
+		//
+		// It's possible that this will always be true, but at this very moment
+		// I think the extra condition to ensure no shenanigans is fine.
+		if (_ws === newWs) {
+			// Gain "exclusive write access" in a sense; nobody else will write to the
+			// `_ws` variable, and any time someone tries to get the websocket, they are
+			// instead directed to a promise that doesn't resolve until we say so.
+			// This must happen first, so that user-code called in stream.onclose
+			// doesn't get a handle to the old websocket instead of the new one.
+			let resolveWsCloseRetry: (ws: WebSocket) => void = () => {};
+			_ws = new Promise<WebSocket>((res) => (resolveWsCloseRetry = res));
+
+			// We spread before operating on the values, so that when calling the onclose handler,
+			// we don't modify the `inFlight` map while still iterating over it.
+			[...inFlight.values()].forEach((stream) =>
+				stream.onclose('WebsocketClosed'),
+			);
+
+			// Wait 2 seconds before retrying; then, before retrying, make sure to
+			// set `_ws` to null so that getWs() actually overwrites the variable again
+			// and we don't infinite loop.
+			setTimeout(() => {
+				_ws = null;
+				getWs().then(resolveWsCloseRetry);
+			}, 2000);
+		}
+	};
+
+	ws.onmessage = (evt) => {
+		const data = JSON.parse(evt.data);
+		const stream = inFlight.get(data.id);
+		if (!stream) {
+			return;
 		}
 
-		console.log('making new websocket');
-		let resolveWs: (ws: WebSocket) => void = () => {};
+		try {
+			switch (data.kind) {
+				case 'error':
+					stream.onerror(data.data, 'MethodError');
+					break;
 
-		const newWs = new Promise<WebSocket>((res) => (resolveWs = res));
-		_ws = newWs;
+				case 'methodDone':
+					stream.onclose('MethodDone');
+					break;
 
-		const ws = new WebSocket(
-			`ws://${window.location.hostname}:9010/api/websocket`,
-		);
-		ws.onclose = (evt) => {
-			console.log('close', evt.code);
-
-			// We only want to over-write if this specific websocket is the one that
-			// is currently active; otherwise, we can just die peacefully.
-			//
-			// It's possible that this will always be true, but at this very moment
-			// I think the extra condition to ensure no shenanigans is fine.
-			if (_ws === newWs) {
-				// Gain "exclusive write access" in a sense; nobody else will write to the
-				// `_ws` variable, and any time someone tries to get the websocket, they are
-				// instead directed to a promise that doesn't resolve until we say so.
-				// This must happen first, so that user-code called in stream.onclose
-				// doesn't get a handle to the old websocket instead of the new one.
-				let resolveWsCloseRetry: (ws: WebSocket) => void = () => {};
-				_ws = new Promise<WebSocket>((res) => (resolveWsCloseRetry = res));
-
-				// We spread before operating on the values, so that when calling the onclose handler,
-				// we don't modify the `inFlight` map while still iterating over it.
-				[...inFlight.values()].forEach((stream) =>
-					stream.onclose('WebsocketClosed'),
-				);
-
-				// Wait 2 seconds before retrying; then, before retrying, make sure to
-				// set `_ws` to null so that getWs() actually overwrites the variable again
-				// and we don't infinite loop.
-				setTimeout(() => {
-					_ws = null;
-					getWs().then(resolveWsCloseRetry);
-				}, 2000);
+				default:
+					stream.onmessage(data);
 			}
-		};
-
-		ws.onmessage = (evt) => {
-			const data = JSON.parse(evt.data);
-			const stream = inFlight.get(data.id);
-			if (!stream) {
-				return;
-			}
-
-			try {
-				switch (data.kind) {
-					case 'error':
-						stream.onerror(data.data, 'MethodError');
-						break;
-
-					case 'methodDone':
-						stream.onclose('MethodDone');
-						break;
-
-					default:
-						stream.onmessage(data);
-				}
-			} catch (e) {
-				stream.onerror(e, 'HandlerException');
-			}
-		};
-
-		ws.onerror = (evt) => {
-			console.error('Robin WS error', evt);
-		};
-
-		ws.onopen = () => {
-			console.log('websocket opened');
-			resolveWs(ws);
-		};
-
-		return _ws;
+		} catch (e) {
+			stream.onerror(e, 'HandlerException');
+		}
 	};
-})();
+
+	ws.onerror = (evt) => {
+		console.error('Robin WS error', evt);
+	};
+
+	ws.onopen = () => {
+		console.log('websocket opened');
+		resolveWs(ws);
+	};
+
+	return _ws;
+}
 
 type StreamCloseCause = 'CalledCloseMethod' | 'WebsocketClosed' | 'MethodDone';
 type StreamErrorSource = 'HandlerException' | 'MethodError';
